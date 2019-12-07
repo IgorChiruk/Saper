d94687f6650748b1d77123fbc461d105f6a4a0c5
Igor Chiruk <mer13@tut.by>
2019-12-07 17:11:50 +0300

    готовые окна

---
+++ Saper/select_diff.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Saper
+{
+    /// <summary>
+    /// Логика взаимодействия для select_diff.xaml
+    /// </summary>
+    public partial class select_diff : Window
+    {
+        public select_diff()
+        {
+            InitializeComponent();
+        }
+
+        private void Lite_diff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mid_diff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Hard_diff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
